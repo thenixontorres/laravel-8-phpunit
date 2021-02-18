bda3de06ac30c1d3e6d2caa1ea8ea8b5e0f67b83
thenixontorres <nixontorres2@gmail.com>
2021-02-18 18:21:36 -0400

    lesson 11

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,4 @@ Route::get('/about', function () {
 
 Route::view('profile','profile');
 
-Route::post('profile',function(Illuminate\Http\Request $request){
-    $request->file('photo')->store('profiles');
-    return redirect('profile');
-});
+Route::post('/profile', [App\Http\Controllers\ProfileController::class,'upload']);
